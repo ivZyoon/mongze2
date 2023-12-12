bebeb94f761d4147b27017d6425829533f9e532b
kimjiyoon <kimjiyoon@innerviewit.com>
2023-12-12 15:18:02 +0900

    [피드백 내용 수정]

--- script.js
+++ script.js
@@ -1,92 +1,74 @@
-var swiper = new Swiper('.swiper-container', {
+$(document).ready(function(){
+    
+  var mainSlideSwiper = new Swiper('.main_banner_container', {
 
-  direction: "horizontal",
-  slidesPerView : 3,
-  spaceBetween: 20,
-  observer: true,
-  observeParents: true,
-  //autoplay: true, 
-
-  pagination: {
-      el: ".swiper-pagination",
-      type: "fraction",
-  },
-
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-
-  on: {
-    init: function(){
-          console.log("swiper 초기화 될 때 실행");
+      direction: "horizontal",
+      slidesPerView : 3,
+      spaceBetween: 20,
+      observer: true,
+      observeParents: true,
+      autoplay: {
+        delay : 3000,
+          disableOnInteraction: false,
       },
-      imagesReady : function(){ // 모든 내부 이미지가 로드된 직후 이벤트 시작
-          console.log("슬라이드 이미지 로드 후 실행");
+      watchOverflow: false,
+      
+      pagination: {
+          el: ".main_banner_pagination",
+          type: "fraction",
       },
-  },
-}); 
+
+      navigation: {
+          nextEl: ".main_banner_btn_next",
+          prevEl: ".main_banner_btn_prev",
+      },
+  }); 
 
 
+  /**
+  * 해시태그 가져와서 넣기
+  */
+  var hashtagTitle = $('.df-bannermanager-kjy-test-banner').find('a:eq(0)').find('span.banner_title').html().split(','); // 첫 번째 배너 : 해시태그 내용 가져오는 용도
 
-$('.df-bannermanager-kjy-test-banner').children('a:eq(0)').addClass('displaynone'); // 첫번째 배너 숨기기
-
-/**
-* 해시태그 가져와서 넣기
-*/
-var hashtagTitle = $('.df-bannermanager-kjy-test-banner').find('a:eq(0)').find('span.banner_title').html().split(',');
-$('.main_hashtag').children('span').each(function(index){
-  $(this).html('#' + hashtagTitle[index]);
-});
-
-
-/**
-* 배너 타이틀 수정 / 해시태그 클릭 이벤트('.active') / 해시태그와 같은 배너 찾기
-*/
-var bannerTitleGroup = $('.df-bannermanager-kjy-test-banner').find('span.banner_title');
-
-bannerTitleGroup.each(function(){
-  var bannerTitleSplit = $(this).html().split('$');
-  $(this).html(bannerTitleSplit[0]); // 배너별 타이틀에서 해시태그 부분은 안보이게 html 수정
-  
-  $(this).closest('a.main_banner_item').attr('data-hashtag', bannerTitleSplit[1]);
-  
-});
-
-
-$('.main_hashtag').children('span').on('click', function(){
-  var hashtagName = $(this).html();
-  // 해시태그 클릭 이벤트 (.active)
-  $('.main_hashtag').children('span').each(function(){
-
-      $(this).removeClass('active');
-  })
-  
-  $(this).addClass('active');
-  
-  $("[data-hashtag*='" + hashtagName + "']").removeClass('displaynone');
-  $('a.main_banner_item').not("[data-hashtag*='" + hashtagName + "']").addClass('displaynone');
-  
-  
-  if(hashtagName == "#전체"){
-    $('a.main_banner_item').removeClass('displaynone');
-      $('a.main_banner_item:first-of-type').addClass('displaynone');
-  }
-  
-  // 그치만 안된다.....
-$(swiper).on('init', function(){
-    console.log('slide가 초기화 설정을 마친 후 실행');
+  $.each(hashtagTitle, function(index, value){
+      $('.main_hashtag').append($('<span>' + '#' + value + '</span>')); // .main_hashtag의 자식으로 해시태그 내용들 span으로 추가
   });
-  console.log("초기화가 됐어요");
+
+
+  /**
+  * 배너 타이틀 수정
+  */
+  var bannerTitleGroup = $('.df-bannermanager-kjy-test-banner').find('span.banner_title');
+
+  bannerTitleGroup.each(function(){
+      var bannerTitleSplit = $(this).html().split('$'); // $를 기준으로 배너 타이틀 자르기
+      $(this).html(bannerTitleSplit[0]); // 배너별 타이틀에서 해시태그 부분빼서 html에 넣기
+
+      $(this).closest('a.main_banner_item').attr('data-hashtag', bannerTitleSplit[1]); // 배너 타이틀에서 $를 기준으로 잘라낸 해시태그 부분 data-hashtag로 부여
+
+  });
+
+
+  /*
+  * 해시태그 클릭 이벤트 (하이라이트 + 클릭한 해시태그와 data-hashtag와의 일치 여부 확인)
+  */
+  $('.main_hashtag').children('span').on('click', function(){
+      var hashtagName = $(this).html(); // 클릭한 해시태그 이름
+
+      // 해시태그 클릭 이벤트 (.active)
+      $('.main_hashtag span').removeClass('active');
+      $(this).addClass('active'); // 핑크 하이라이트 넣기
+
+      $('a.main_banner_item').removeClass('displaynone');
+      $('a.main_banner_item').not("[data-hashtag*='" + hashtagName + "']").addClass('displaynone'); //클릭한 해시태그 값과 data-hashtag 값이 일치하지 않으면 숨기기
+
+      if(hashtagName == "#전체"){
+          $('a.main_banner_item').removeClass('displaynone'); // 전체태그에서는 모든 슬라이드 보여주기
+          $('a.main_banner_item:first-of-type').addClass('displaynone');  // 해시태그 가져오기용 첫번째 배너만 숨기기
+      }
+
+      mainSlideSwiper.update(); // 메인 슬라이드의 스와이퍼 초기화
+  });
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
